040febb405a8fc775efaae26d5ac136f61cd0ecf
Karen Ventura <ventura.802@hotmail.com>
2016-11-11 09:16:39 -0600

    Remove unneccesary column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161106235249) do
+ActiveRecord::Schema.define(version: 20161111145929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20161106235249) do
     t.string   "type"
     t.integer  "category_id"
     t.integer  "user_id"
-    t.string   "schedule_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end

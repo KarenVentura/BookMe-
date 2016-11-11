abe21a22ccf16d87a64f8aa73db6186286964111
Karen Ventura <ventura.802@hotmail.com>
2016-11-11 09:17:16 -0600

    Add default data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+gynecology = Category.find_or_create_by(name: "Gynecology")
+Category.find_or_create_by(name: "Dentist")
+Category.find_or_create_by(name: "Doctor")
+Category.find_or_create_by(name: "Mechanic")
+Category.find_or_create_by(name: "Psychologist")
+Category.find_or_create_by(name: "Dermatologist")
+Category.find_or_create_by(name: "Spa")
+
+user = User.create(name: "Karen", last_name: "Ventura", email: "ventura@gmail.com", telephone: "1231231231", cellphone: "1231231234")
+
+Service.find_or_create_by(name: "Gynecology Gomez", description: "test", week_days: "Monday, Friday", category_id: gynecology.id, user_id: user.id)

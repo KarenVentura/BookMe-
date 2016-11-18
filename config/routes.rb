21b675d9050d2ea01e6b0b0f606bea840d1436e8
Karen Ventura <ventura.802@hotmail.com>
2016-11-17 22:10:10 -0600

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   constraints subdomain: 'api' do
-    namespace :api, path: '/' do
-      resources :service do
-        resources :appointment do
-          resources :review, only: [:index, :new, :show, :edit, :update]
+    scope module: 'api' do
+      namespace :v1 do
+        resources :service do
+          resources :appointment do
+            resources :review, only: [:index, :new, :show, :edit, :update]
+          end
+          resources :schedule
+          resources :category
         end
-        resources :schedule
-        resources :category
-      end
 
-      resources :user do
-        resource :perfil
+        resources :user do
+          resource :perfil
+        end
       end
     end
   end

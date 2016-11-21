f397840cfa6245ce4720f5c04e6d29aeecc2e1d9
Karen Ventura <ventura.802@hotmail.com>
2016-11-20 21:40:17 -0600

    Add service selializer

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -1,3 +1,3 @@
 class ServiceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :week_days, :category_id, :user_id
+  attributes :id, :name, :description, :week_days, :specialty, :category_id, :user_id
 end

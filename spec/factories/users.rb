bbce242de8a2dd65308700f6d5ad7975117d7a23
Karen Ventura <ventura.802@hotmail.com>
2016-12-04 21:12:55 -0600

    Add user resource tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,10 @@ FactoryGirl.define do
   factory :user do
     name "Karen"
     last_name "Ventura"
-    email "ventura@gmail.com"
+    email { FFaker::Internet.email }
     telephone "1231231231"
     cellphone "1231231234"
+    password "12345678"
+    password_confirmation "12345678"
   end
 end

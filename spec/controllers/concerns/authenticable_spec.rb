9ff8aa002146384c615a180cb70fe22116ed1df3
Karen Ventura <ventura.802@hotmail.com>
2016-12-06 07:52:14 -0600

    Adds authenticable module for managing authentication methods

---
+++ spec/controllers/concerns/authenticable_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+class Authentication
+  include Authenticable
+end
+
+describe Authenticable do
+  let(:authentication) { Authentication.new }
+  let!(:user) { FactoryGirl.create(:user) }
+  subject { authentication }
+
+  describe "#current_user" do
+    before do
+      request.headers["Authorization"] = user.auth_token
+      authentication.stub(:request).and_return(request)
+    end
+
+    it "returns the user from the authorization header" do
+      expect(authentication.current_user.auth_token).to eql userr.auth_token
+    end
+  end
+end
